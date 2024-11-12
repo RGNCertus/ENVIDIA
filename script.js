89ddb84786673e5a8a4b046173a6d9a783939f73
andre2ie7e <andresfg1499@gmail.com>
2024-11-11 23:33:39 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
+let index = 0;
+const imagenes = document.querySelectorAll('.imagen-carrusel');
+const totalImagenes = imagenes.length;
 
+function moverCarrusel() {
+    index++;
+    if (index >= totalImagenes) {
+        index = 0; 
+    }
+ 
+    document.querySelector('.carrusel-imagenes').style.transform = `translateX(-${index * 100}%)`;
+}
+
+setInterval(moverCarrusel, 3000);
