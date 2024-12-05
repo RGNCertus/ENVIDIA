20902e358df365da56e20fed1c9ed030656c47df
andre2ie7e <andresfg1499@gmail.com>
2024-12-05 00:49:45 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,30 +1,3 @@
-let index = 0;  
-const imagenes = document.querySelectorAll('.imagen-carrusel');  
-const totalImagenes = imagenes.length;  
-
-function mostrarImagen() {
-
-    imagenes[index].classList.remove('activa');
-    
-    index++;
-    
-    if (index >= totalImagenes) {
-        index = 0;
-    }
-    
-    imagenes[index].classList.add('activa');
-}
-
-mostrarImagen();
-setInterval(mostrarImagen, 3000);
-
-/* Catalogo de Productos*/
-document.querySelectorAll('.producto button').forEach(button => {
-    button.addEventListener('click', function() {
-        alert('Producto añadido al carrito');
-    });
-}); 
-
 
 // Variables globales
 let carrito = [];
